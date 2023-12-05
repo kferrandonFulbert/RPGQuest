d11857a0e6d1de989c827397abbfe1bb9e59ecf9
kevin FERRANDON <98470782+kferrandonFulbert@users.noreply.github.com>
2023-12-05 17:52:26 +0100

    first add

---
+++ RPGQuest/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace RPGQuest
+{
+    partial class frmRPG
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxArmes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbArmures = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.picArme = new System.Windows.Forms.PictureBox();
+            this.picArmure = new System.Windows.Forms.PictureBox();
+            this.cbClasses = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.picPersonnage = new System.Windows.Forms.PictureBox();
+            this.pgbVie = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pgbNiveau = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pgbExperience = new System.Windows.Forms.ProgressBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.picPieces = new System.Windows.Forms.PictureBox();
+            this.lblpieces = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pgbForce = new System.Windows.Forms.ProgressBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pgbMagie = new System.Windows.Forms.ProgressBar();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pgbDefense = new System.Windows.Forms.ProgressBar();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnPlusForce = new System.Windows.Forms.Button();
+            this.btnPlusMagie = new System.Windows.Forms.Button();
+            this.btnPlusDefense = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picArme)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picArmure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPersonnage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPieces)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxArmes
+            // 
+            this.comboBoxArmes.FormattingEnabled = true;
+            this.comboBoxArmes.Location = new System.Drawing.Point(38, 39);
+            this.comboBoxArmes.Name = "comboBoxArmes";
+            this.comboBoxArmes.Size = new System.Drawing.Size(196, 21);
+            this.comboBoxArmes.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Armes";
+            // 
+            // cbArmures
+            // 
+            this.cbArmures.FormattingEnabled = true;
+            this.cbArmures.Location = new System.Drawing.Point(38, 314);
+            this.cbArmures.Name = "cbArmures";
+            this.cbArmures.Size = new System.Drawing.Size(202, 21);
+            this.cbArmures.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Armures";
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(1023, 731);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "&Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // picArme
+            // 
+            this.picArme.Location = new System.Drawing.Point(38, 87);
+            this.picArme.Name = "picArme";
+            this.picArme.Size = new System.Drawing.Size(196, 175);
+            this.picArme.TabIndex = 5;
+            this.picArme.TabStop = false;
+            // 
+            // picArmure
+            // 
+            this.picArmure.Location = new System.Drawing.Point(38, 362);
+            this.picArmure.Name = "picArmure";
+            this.picArmure.Size = new System.Drawing.Size(202, 175);
+            this.picArmure.TabIndex = 6;
+            this.picArmure.TabStop = false;
+            // 
+            // cbClasses
+            // 
+            this.cbClasses.FormattingEnabled = true;
+            this.cbClasses.Location = new System.Drawing.Point(38, 583);
+            this.cbClasses.Name = "cbClasses";
+            this.cbClasses.Size = new System.Drawing.Size(202, 21);
+            this.cbClasses.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 567);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Classes";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(361, 39);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(195, 20);
+            this.txtNom.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(358, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nom";
+            // 
+            // picPersonnage
+            // 
+            this.picPersonnage.Location = new System.Drawing.Point(361, 87);
+            this.picPersonnage.Name = "picPersonnage";
+            this.picPersonnage.Size = new System.Drawing.Size(419, 517);
+            this.picPersonnage.TabIndex = 11;
+            this.picPersonnage.TabStop = false;
+            // 
+            // pgbVie
+            // 
+            this.pgbVie.Location = new System.Drawing.Point(823, 87);
+            this.pgbVie.Name = "pgbVie";
+            this.pgbVie.Size = new System.Drawing.Size(258, 23);
+            this.pgbVie.TabIndex = 13;
+            this.pgbVie.Value = 100;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(820, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Vie";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(820, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Niveau";
+            // 
+            // pgbNiveau
+            // 
+            this.pgbNiveau.Location = new System.Drawing.Point(823, 160);
+            this.pgbNiveau.Name = "pgbNiveau";
+            this.pgbNiveau.Size = new System.Drawing.Size(258, 23);
+            this.pgbNiveau.TabIndex = 15;
+            this.pgbNiveau.Value = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(820, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Experience";
+            // 
+            // pgbExperience
+            // 
+            this.pgbExperience.Location = new System.Drawing.Point(823, 239);
+            this.pgbExperience.Name = "pgbExperience";
+            this.pgbExperience.Size = new System.Drawing.Size(258, 23);
+            this.pgbExperience.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(820, 404);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Pieces";
+            // 
+            // picPieces
+            // 
+            this.picPieces.InitialImage = global::RPGQuest.Properties.Resources.money;
+            this.picPieces.Location = new System.Drawing.Point(823, 429);
+            this.picPieces.Name = "picPieces";
+            this.picPieces.Size = new System.Drawing.Size(196, 175);
+            this.picPieces.TabIndex = 21;
+            this.picPieces.TabStop = false;
+            // 
+            // lblpieces
+            // 
+            this.lblpieces.AutoSize = true;
+            this.lblpieces.Location = new System.Drawing.Point(881, 404);
+            this.lblpieces.Name = "lblpieces";
+            this.lblpieces.Size = new System.Drawing.Size(64, 13);
+            this.lblpieces.TabIndex = 22;
+            this.lblpieces.Text = "Chargement";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(820, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "Force";
+            // 
+            // pgbForce
+            // 
+            this.pgbForce.Location = new System.Drawing.Point(823, 308);
+            this.pgbForce.Name = "pgbForce";
+            this.pgbForce.Size = new System.Drawing.Size(57, 23);
+            this.pgbForce.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(900, 292);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Magie";
+            // 
+            // pgbMagie
+            // 
+            this.pgbMagie.Location = new System.Drawing.Point(903, 308);
+            this.pgbMagie.Name = "pgbMagie";
+            this.pgbMagie.Size = new System.Drawing.Size(57, 23);
+            this.pgbMagie.TabIndex = 25;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(978, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Défense";
+            // 
+            // pgbDefense
+            // 
+            this.pgbDefense.Location = new System.Drawing.Point(981, 308);
+            this.pgbDefense.Name = "pgbDefense";
+            this.pgbDefense.Size = new System.Drawing.Size(57, 23);
+            this.pgbDefense.TabIndex = 27;
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(1059, 314);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(64, 13);
+            this.lblPoints.TabIndex = 30;
+            this.lblPoints.Text = "Chargement";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(1059, 292);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(36, 13);
+            this.label13.TabIndex = 29;
+            this.label13.Text = "Points";
+            // 
+            // btnPlusForce
+            // 
+            this.btnPlusForce.Location = new System.Drawing.Point(823, 348);
+            this.btnPlusForce.Name = "btnPlusForce";
+            this.btnPlusForce.Size = new System.Drawing.Size(19, 23);
+            this.btnPlusForce.TabIndex = 31;
+            this.btnPlusForce.Text = "+";
+            this.btnPlusForce.UseVisualStyleBackColor = true;
+            this.btnPlusForce.Click += new System.EventHandler(this.btnPlusForce_Click);
+            // 
+            // btnPlusMagie
+            // 
+            this.btnPlusMagie.Location = new System.Drawing.Point(903, 348);
+            this.btnPlusMagie.Name = "btnPlusMagie";
+            this.btnPlusMagie.Size = new System.Drawing.Size(19, 23);
+            this.btnPlusMagie.TabIndex = 32;
+            this.btnPlusMagie.Text = "+";
+            this.btnPlusMagie.UseVisualStyleBackColor = true;
+            // 
+            // btnPlusDefense
+            // 
+            this.btnPlusDefense.Location = new System.Drawing.Point(981, 348);
+            this.btnPlusDefense.Name = "btnPlusDefense";
+            this.btnPlusDefense.Size = new System.Drawing.Size(19, 23);
+            this.btnPlusDefense.TabIndex = 33;
+            this.btnPlusDefense.Text = "+";
+            this.btnPlusDefense.UseVisualStyleBackColor = true;
+            // 
+            // frmRPG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 797);
+            this.Controls.Add(this.btnPlusDefense);
+            this.Controls.Add(this.btnPlusMagie);
+            this.Controls.Add(this.btnPlusForce);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.pgbDefense);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.pgbMagie);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pgbForce);
+            this.Controls.Add(this.lblpieces);
+            this.Controls.Add(this.picPieces);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pgbExperience);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pgbNiveau);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pgbVie);
+            this.Controls.Add(this.picPersonnage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbClasses);
+            this.Controls.Add(this.picArmure);
+            this.Controls.Add(this.picArme);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbArmures);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxArmes);
+            this.Name = "frmRPG";
+            this.Text = "RPG QUEST";
+            this.Load += new System.EventHandler(this.frmRPG_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picArme)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picArmure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPersonnage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPieces)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxArmes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbArmures;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.PictureBox picArme;
+        private System.Windows.Forms.PictureBox picArmure;
+        private System.Windows.Forms.ComboBox cbClasses;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox picPersonnage;
+        private System.Windows.Forms.ProgressBar pgbVie;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar pgbNiveau;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar pgbExperience;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox picPieces;
+        private System.Windows.Forms.Label lblpieces;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ProgressBar pgbForce;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ProgressBar pgbMagie;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ProgressBar pgbDefense;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnPlusForce;
+        private System.Windows.Forms.Button btnPlusMagie;
+        private System.Windows.Forms.Button btnPlusDefense;
+    }
+}
+
---
+++ RPGQuest/Form1.cs
@@ -0,0 +1,105 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace RPGQuest
+{
+    public partial class frmRPG : Form
+    {
+        // Initialisation des variables globales
+        private MySqlConnection connection;
+        private int nbPiece = 500;
+        private int nbPointsRepartitions=20;
+        public frmRPG()
+        {
+            InitializeComponent();
+            // Initialisation et connection a la DB
+            InitializeDatabaseConnection();
+            // chargement de la combo Arme depuis la base de données
+            loadArmes();
+            // Initialisation des champs libellés par défaut
+            lblpieces.Text = nbPiece.ToString();
+            lblPoints.Text = nbPointsRepartitions.ToString();
+            // Chargement de l'image et ajustement
+            picPieces.Image = Image.FromFile(@"money.png");
+            picPieces.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void frmRPG_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeDatabaseConnection()
+        {
+            string connectionString = "Server=localhost;Database=RPGQuest;User ID=root;Password=;";
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connexion à la base de données réussie!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur de connexion à la base de données : {ex.Message}", "Erreur de connexion");
+            }
+        }
+
+        public class Arme
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+            public object Type { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+        private void loadArmes()
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT id, nom, type FROM armes", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int id = Convert.ToInt32(row["id"]);
+                    string nom = row["nom"].ToString();
+                    string type = row["type"].ToString();
+
+                    // Ajoutez un élément à la combobox en utilisant le nom de l'arme
+                    // et conservez l'id de l'arme comme valeur associée à l'élément.
+                    Arme uneArme = new Arme { Text = nom, Value = id, Type = type };
+                    comboBoxArmes.Items.Add(uneArme);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors du chargement des données : {ex.Message}", "Erreur de chargement");
+            }
+        }
+
+        private void btnPlusForce_Click(object sender, EventArgs e)
+        {
+            if (pgbForce.Value > 0)
+            {
+                pgbForce.Value = pgbForce.Value + 1;
+
+            }
+        }
+    }
+}
